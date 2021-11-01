9ae39ac555c2338dd727cff917efc5db01fe2da3
Sunil <soninsunil@gmail.com>
2021-11-01 07:40:14 +0530

    sample test message

--- vars/integrationTest.groovy
+++ vars/integrationTest.groovy
@@ -2,15 +2,19 @@ import groovy.json.*
 import groovy.json.JsonSlurperClassic
 import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 
-def run(Map config)
-{
-  def safeParams = config ?: [:]
-
-
-  stage("SpecFlow Test Execution"){
-  checkout scm
-  // sh 'dotnet build'
-  // sh 'dotnet tool install --global SpecFlow.Plus.LivingDoc.CLI'
-	echo 'hello'
-  }
+def run(message) {
+  echo "INFO: ${message}"
 }
+// def run(Map config)
+// {
+
+//   // def safeParams = config ?: [:]
+
+
+//   // stage("SpecFlow Test Execution"){
+//   // checkout scm
+//   // // sh 'dotnet build'
+//   // // sh 'dotnet tool install --global SpecFlow.Plus.LivingDoc.CLI'
+// 	// echo 'hello'
+//   // }
+// }
