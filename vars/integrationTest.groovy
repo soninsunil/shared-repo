02cc1614ae1d88a91d6067c20a42009d6b9dba88
Sunil <soninsunil@gmail.com>
2021-11-01 08:35:00 +0530

    updated final function

--- vars/integrationTest.groovy
+++ vars/integrationTest.groovy
@@ -5,16 +5,3 @@ import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
 def run(message) {
   echo "INFO: ${message}"
 }
-// def run(Map config)
-// {
-
-//   // def safeParams = config ?: [:]
-
-
-//   // stage("SpecFlow Test Execution"){
-//   // checkout scm
-//   // // sh 'dotnet build'
-//   // // sh 'dotnet tool install --global SpecFlow.Plus.LivingDoc.CLI'
-// 	// echo 'hello'
-//   // }
-// }
